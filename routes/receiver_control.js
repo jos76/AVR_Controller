40cdb132852d9b4a1c334a30009131a57b93aa55
Jon Savage <jhsavage76@gmail.com>
2018-06-15 08:43:00 -0400

    updated endpoint

--- routes/receiver_control.js
+++ routes/receiver_control.js
@@ -7,15 +7,15 @@ var baseUrl = 'http://192.168.1.248/';
 var getUrl = baseUrl + 'goform/formMainZone_MainZoneXml.xml';
 var postUrl = baseUrl + 'MainZone/index.put.asp';
 
-/* receiver_control/post */
-router.post('/post', function(req, res, next) {
+/* receiver_control/ */
+router.post('/', function(req, res, next) {
   var form = req.body;
   res.sendStatus(200); // send status before receiver request b/c receiver request takes a second or two
   request.post({url: postUrl, form: form}, function() {console.log('Sent Receiver: ' + JSON.stringify(form));});
 });
 
-/* receiver_control/get */
-router.get('/get', function(req, res, next) {
+/* receiver_control/ */
+router.get('/', function(req, res, next) {
 
   var zone = req.query.zone;
   var url;
